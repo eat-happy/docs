a4fe44fe2f45ce7c2b50f0667e957f1e9a6e4555
huqing <huqing@zhiketong.cn>
2021-05-29 19:09:12 +0800

    自动构建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,24 @@
 module.exports = {
+    base:'docs',
     themeConfig: {
-        lastUpdated: 'Last Updated',
+        lastUpdated: '上次更新',
         logo: '/蜡笔小新.jpeg',
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'About', link: '/about/' },
-            { text: 'External', link: 'https://baidu.com' },
+            {text: 'Home', link: '/'},
+            {text: 'About', link: '/about/'},
+            {text: 'External', link: 'https://baidu.com'},
             {
                 text: 'Languages',
                 ariaLabel: 'Language Menu',
                 items: [
-                    { text: 'Chinese', link: '/language/chinese/' },
-                    { text: 'Japanese', link: '/language/japanese/' },
+                    {text: 'Chinese', link: '/language/chinese/'},
+                    {text: 'Japanese', link: '/language/japanese/'},
                     {
                         text: 'Languages',
                         ariaLabel: 'Language Menu',
                         items: [
-                            { text: 'Chinese', link: '/language/chinese/' },
-                            { text: 'Japanese', link: '/language/japanese/' }
+                            {text: 'Chinese', link: '/language/chinese/'},
+                            {text: 'Japanese', link: '/language/japanese/'}
                         ]
                     }
                 ]
@@ -26,7 +27,19 @@ module.exports = {
         sidebar: [
             '/',
             '/about/'
-        ]
+        ],
 
     },
+    plugins: [
+        [
+            '@vuepress/last-updated',
+            {
+                transformer: (timestamp, lang) => {
+                    // 不要忘了安装 moment
+                    const moment = require('moment')
+                    moment.locale(lang)
+                    return moment(timestamp).fromNow()
+                }
+            }
+        ]]
 }
