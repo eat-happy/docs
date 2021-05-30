21e37644f05e940adafefbc3dd3e963cad8427a2
huqing <huqing@zhiketong.cn>
2021-05-30 12:46:58 +0800

    pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,16 @@
 module.exports = {
-    base:'/',
+    head: [
+        ['link', {rel: 'icon', href: '/蜡笔小新.jpeg'}],
+        ['link', {rel: 'manifest', href: '/manifest.json'}],
+        ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+        ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+        ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+        ['link', {rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png'}],
+        ['link', {rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c'}],
+        ['meta', {name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png'}],
+        ['meta', {name: 'msapplication-TileColor', content: '#000000'}]
+    ],
+    base: '/',
     themeConfig: {
         lastUpdated: '上次更新',
         logo: '/蜡笔小新.jpeg',
@@ -31,15 +42,19 @@ module.exports = {
 
     },
     plugins: [
-        [
+       [ '@vuepress/pwa', {
+           serviceWorker: true,
+           updatePopup: true
+       }],
+            [
             '@vuepress/last-updated',
-            {
-                transformer: (timestamp, lang) => {
-                    // 不要忘了安装 moment
-                    const moment = require('moment')
-                    moment.locale(lang)
-                    return moment(timestamp).fromNow()
+                {
+                    transformer: (timestamp, lang) => {
+                        // 不要忘了安装 moment
+                        const moment = require('moment')
+                        moment.locale(lang)
+                        return moment(timestamp).fromNow()
+                    }
                 }
-            }
-        ]]
+            ]]
 }
